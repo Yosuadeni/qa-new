3a08f7f19224803a8223f4969dc048a8bd0fba9d
Yosuadeni <yosdeni12@gmail.com>
2023-12-15 21:15:46 +0700

    first commit

--- test/test_routes.py
+++ test/test_routes.py
@@ -2,6 +2,7 @@ import json
 
 from .settings import DEFAULT_PRICE
 
+
 def test_product_detail_api(client):
     id = 3
     response = client.get(f"/api/products/{id}")
@@ -15,3 +16,15 @@ def test_product_api(client):
     response = client.get("/api/products")
     assert response.status_code == 200
 
+
+def test_post_cart(client):
+    url = f"/api/cart"
+    coupon_code = ""
+    shipping_fee = 0
+    cart_items = [{"product_id": 6, "qty": 1}]
+    payload = {"coupon_code": coupon_code,
+               "shipping_fee": shipping_fee, "cart_items": cart_items}
+    response = client.post(url, json=payload)
+    assert response.status_code == 200
+    if response.status_code == 200:
+        assert "data created"
